eb99ff5a96c29437b96bf1e2cfe25904e8b38665
André Dargains <andre.dargains@vodafone.com>
2024-02-20 16:09:26 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import List from "./components/List";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Pokédex</h1>
+      <List></List>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Item from "./Item";
+
+const List = () => {
+  return (
+    <ul>
+      <Item>item</Item>
+    </ul>
+  );
+};
+
+export default List;
